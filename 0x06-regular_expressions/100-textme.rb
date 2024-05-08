a760a0c4c2cbec0f5f7b1489830b99521d88efb7
cyihuoma <122976845+cyihuoma@users.noreply.github.com>
2024-05-08 11:37:19 +0100

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+
+# Accept one argument and pass it to a regular expression matching method
+match_data = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+
+# Output results in the required format
+match_data.each do |match|
+  puts "#{match[0]},#{match[1]},#{match[2]}"
+end
