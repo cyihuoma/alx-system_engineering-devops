499cd3638adf1d88221dbadb2b7d9bed36073aac
cyihuoma <ladyihuoma@gmail.com>
2024-08-29 15:54:29 +0100

    Debugg

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+
+
+# Puppet manifest to fix Apache returning a 500 error
+
+exec { 'fix wordpress':
+	command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path        => '/usr/local/bin:/bin',
+}
+
